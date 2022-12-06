ff14c8bbf292bd235d7309d271c076eab3838875
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2022-12-06 23:39:53 +0800

    每日經文區塊完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,38 @@
-console.log('Hello!');
+const BaseURl = `https://json-server-vercel-seven.vercel.app/`
+
 
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+
+  //GET請求
+axios.get(`${BaseURl}DailyBibles`)
+.then( (response) => console.log(response))
+.catch( (error) => console.log(error))
+
+ 
+
+axios.get(`${BaseURl}Abouts`)
+.then( (response) => console.log(response))
+.catch( (error) => console.log(error))
+
+
+
+axios.get(`${BaseURl}Historys`)
+.then( (response) => console.log(response))
+.catch( (error) => console.log(error))
+
+
+
+axios.get(`${BaseURl}Schedules`)
+.then( (response) => console.log(response))
+.catch( (error) => console.log(error))
+
+
+
+//users
+
+axios.get(`${BaseURl}users`)
+.then( (response) => console.log(response))
+.catch( (error) => console.log(error))
+
+
 });
